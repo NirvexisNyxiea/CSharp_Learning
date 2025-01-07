e703d87522485edaf599da5cc918a629705d18a3
Nirvexis <nirvexis@gmail.com>
2025-01-08 00:10:09 +0300

    Devam

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region for döngüsü
+
+            //x: baslangic degerini tutar 
+            //y; bitis degerini tutar end value
+            //x; artis ya da azalis degelerini tutar increase decrease ===================== onemli unutma==
+
+            //int i;
+
+            //for (i =1; i<=5; i++)
+            //{
+            //    Console.WriteLine("Csharp Egitim kampi");
+            //}
+
+            //for (int i = 1; i<=20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 3; i<= 50; i += 3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write(" Lütfen ekrana yazılmasını istediğiniz adedi giriniz: ");
+            //int maxValue =int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= maxValue; i++)
+            //{
+            //    Console.WriteLine("CSharp eglenceli");
+            //}
+
+
+            //Console.Write("Başlangıç değerini giriniz: ");
+            //int startNum = int.Parse(Console.ReadLine());
+            //Console.Write("Durdurmak istediğiniz değeri giriniz: ");
+            //int endNum = int.Parse(Console.ReadLine());
+            //Console.Write("Kaçar birim artmasını istiyorsanız o değeri giriniz: ");
+            //int incDecrease = int.Parse(Console.ReadLine());
+
+            //for (int i = startNum; i <= endNum; i += incDecrease)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            #endregion
+
+            #region For döngüsü ile karar yapıları
+
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (i%5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}   
+
+            //int totalValue = 0;
+
+            //for (int i = 1; i <=10 ; i++)
+            //{
+            //    totalValue += i;
+            //}
+
+            //Console.WriteLine(totalValue);
+
+            //int totalValue = 0;
+
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        totalValue += i;
+            //    }
+            //}
+
+
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
