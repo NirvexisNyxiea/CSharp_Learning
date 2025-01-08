560ca612c90f47d87f1480443f8c5ba8fb0f518a
Nirvexis <nirvexis@gmail.com>
2025-01-08 22:38:56 +0300

    day 5

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Alta Alta 10 tane yildiz olusturma
+
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+
+            #endregion
+
+
+            #region Yan yana 10 tane yildiz olusturma
+
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+
+            #endregion
+
+            #region Alt alta 10 tane yildiz olusturma her satirda 10 tane yildiz olsun
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //   Console.WriteLine("**********");
+            //}
+            #endregion
+
+            #region dik ucgen
+
+
+            //for (int i = 1; i <=10; i ++ )
+            //{
+            //    for (int j=1; j<=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            #endregion
+
+            #region Ters dik ucgen
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for(int j = 1; j<=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+            #endregion
+
+            #region MyRegion
+
+            
+            #endregion
+            Console.Read();
+        }
+    }
+}
